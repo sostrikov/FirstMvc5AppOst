0ab226dd6295af60e47c3d07a13313093e65ede3
ostrikov <ostrikov@bmstu.ru>
2019-12-17 17:07:36 +0300

    initial

---
+++ FirstMvc5App/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMvc5App.Models;//Нужно подключить
+
+namespace FirstMvc5App.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+            // возвращаем представление
+            return View();
+        }
+    }
+}
